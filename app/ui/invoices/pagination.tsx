2b47861821222e5b239ee2e2305775cdfbd8a05b
har-sat <harsat.ponnusamy@gmail.com>
2025-05-03 01:01:47 +0530

    tut11 search only

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
-  // NOTE: Uncomment this code in Chapter 11
+
 
   // const allPages = generatePagination(currentPage, totalPages);
 
